41b71087acdbb48fe31e48c87cb18ae37e34f5c7
John Z. <johnziss9@gmail.com>
2025-12-24 14:00:03 +0000

    Added movies controller with endpoints to get all, get by id and add a movie. Updated Test project to .Net 10.

---
+++ backend/SceneStack.API/Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SceneStack.API.Data;
+using SceneStack.API.Models;
+
+namespace SceneStack.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MoviesController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public MoviesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/movies
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+    {
+        return await _context.Movies.ToListAsync();
+    }
+
+    // GET: api/movies/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Movie>> GetMovie(int id)
+    {
+        var movie = await _context.Movies.FindAsync(id);
+
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return movie;
+    }
+
+    // POST: api/movies
+    [HttpPost]
+    public async Task<ActionResult<Movie>> CreateMovie(Movie movie)
+    {
+        _context.Movies.Add(movie);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+    }
+}
